2a244ab2a2b90b229017b71299fbc32c3a70c72b
Jackson Williams <jacksonwilliams.williams@gmail.com>
2024-09-07 15:18:49 -0600

    changes all references to trivia blanca to dickerwill paradox

--- dicker-will-paradox/README.md
+++ dicker-will-paradox/README.md
@@ -1,4 +1,4 @@
-# Triviablanca2
+# Dickerwillparadox
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.2.
 
--- dicker-will-paradox/src/app/app.component.spec.ts
+++ dicker-will-paradox/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'triviablanca2' title`, () => {
+  it(`should have the 'dickerwillparadox' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('triviablanca2');
+    expect(app.title).toEqual('dickerwillparadox');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, triviablanca2');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, dickerwillparadox');
   });
 });
